28c524afa782aee14924ba2934299de2757f7909
SaiPranav Madupu <msaipranav2004@gmail.com>
2026-02-04 15:37:37 +0530

    Updates

---
+++ _next/static/0y4QXfB-Pt53rMYk6dg4p/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio-pranav//_next/:path+",
+        "destination": "/portfolio-pranav/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
