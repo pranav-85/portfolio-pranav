56257d70e907719b6cacafc18169ffd6ebac9e4d
pranav <125111881+pranav-85@users.noreply.github.com>
2026-02-05 10:01:36 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/portfolio-pranav",
-  assetPrefix: "/portfolio-pranav/",
+  basePath: "/",
+  assetPrefix: "/",
   images: {
     unoptimized: true,
   },
